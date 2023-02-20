dc34b2a3df7994efcd0ca7376b8331137a9f7717
ootkungdevto666 <119470333+ootkungdevto666@users.noreply.github.com>
2023-02-20 10:17:03 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -22,20 +24,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String text = "";
+  List<Map<String, dynamic>> Person_data = [
+    {"id": 1, "name": "jirapas", "age": 23},
+    {"id": 2, "name": "miyu", "age": 22},
+    {"id": 3, "name": "sisbel", "age": 19},
+    {"id": 4, "name": "jirapas", "age": 23},
+    {"id": 5, "name": "miyu", "age": 22},
+    {"id": 6, "name": "sisbel", "age": 19},
+    {"id": 7, "name": "jirapas", "age": 23},
+    {"id": 8, "name": "miyu", "age": 22},
+    {"id": 9, "name": "sisbel", "age": 19},
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    text = Person_data.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("MyHome"),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
-            child: Text("Hello World"),
+            child: Text(""),
+          ),
+          SizedBox(
+            height: 250,
+            child: Column(
+              children: [SingleChildScrollView(
+                child: buildlist(),
+              )],
+            ),
           )
         ],
       ),
+    );
+  }
+
+  Widget buildlist() {
+    return Column(
+      children: Person_data.map(
+        (json) => Card(
+          child: ListTile(
+            title: Text("${json["id"]} : ${json["name"]}"),
+            subtitle: Text("${json["age"]}"),
+            trailing: Text("${json["age"]}"),
+          ),
+        ),
+      ).toList(),
     );
   }
 }
